f11367d60f7951d2ad685f8db001b1671339bc89
hfish796-boop <hfish796@gmail.com>
2026-01-31 23:35:08 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+function CounterTitle() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  }, [count]);
+
+  return (
+    <div>
+      <h2>Counter</h2>
+      <p>Count: {count}</p>
+
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+    </div>
+  );
+}
+
+export default CounterTitle;
