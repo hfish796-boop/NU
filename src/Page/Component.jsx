364b8f89c33d6518b0ee9b85d591e8a34bb49d6f
hfish796-boop <hfish796@gmail.com>
2026-02-01 09:25:32 +0700

    save

--- src/Page/Component.jsx
+++ src/Page/Component.jsx
@@ -1,23 +1,23 @@
-// import React from 'react'
-// import PropTypes from 'prop-types'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-// function Component  ({name,age,bio})  {
-//   return (
-//     <>
-//     <div>
-//         <h2>{name}</h2>
-//         <p>{age}</p>
-//         <p>{bio}</p>
-//     </div>
-//     </>
-//   )
-// }
-// Component.propTypes={
-//     name: PropTypes.string.isRequired,
-//     age: PropTypes.number.isRequired,
-//     bio: PropTypes.string,
-// }
-// Component.defaultProps={
-//     bio:`This user has not provided a bio`
-// }
-// export default Component
+function Component  ({name,age,bio})  {
+  return (
+    <>
+    <div>
+        <h2>{name}</h2>
+        <p>{age}</p>
+        <p>{bio}</p>
+    </div>
+    </>
+  )
+}
+Component.propTypes={
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired,
+    bio: PropTypes.string,
+}
+Component.defaultProps={
+    bio:`This user has not provided a bio`
+}
+export default Component
